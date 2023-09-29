7faa7cf031f39786c9d0ea73288c903cbc24d60c
zimula <zimula@live.dk>
2023-09-29 17:24:43 +0200

    1st delegate example

---
+++ intro/Classes&Objects/Delegates/Methods/MethodsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Methods
+{
+    internal class MethodsClass
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return (a > b) ? (a - b) : (b - a);
+        }
+    }
+}
---
+++ intro/Classes&Objects/Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using Delegates.Methods;
+namespace Delegates
+{
+    delegate int MathFunc(int a, int b);
+    internal class Program
+    {
+        /*
+            A simple app that relies on a delegate to call methods. 
+                - delegates work as references to methods. 
+         */
+        static void Main(string[] args)
+        {
+            //calling delegate 
+            MathFunc mfa = new MathFunc(MethodsClass.Add);
+
+            Console.WriteLine("Delegates in action!");
+            Console.WriteLine("Addition result: "+ " " + mfa(4,5));
+            mfa = new MathFunc(MethodsClass.Sub);
+            Console.WriteLine("Subtraction result:" + " " +  mfa(4,5));
+            // as illustrated, a delegate can be used for multiple methods, as long as they have the same signature. 
+        }
+    }
+}
